5c9690e2ce48be23c2ab9509bc74e1a844a7a448
Kim Streich <kim@infusedinsight.com>
2023-04-26 20:10:31 -0600

    Text FP: Added

---
+++ ergogen/footprints/text.js
@@ -0,0 +1,41 @@
+module.exports = {
+  params: {
+    designator: 'TXT',
+    side: 'F',
+    reverse: false,
+    text: 'Awesomeness',
+  },
+  body: p => {
+    const top = `
+      (module text (layer F.Cu) (tedit 6449CD11)
+        ${p.at /* parametric position */}
+    `
+    const front = `
+      (fp_text user "${p.text}" (at 0 0) (layer F.SilkS)
+        (effects (font (size 1 1) (thickness 0.15)))
+      )
+    `
+    const back = `
+      (fp_text user "${p.text}" (at 0 0) (layer B.SilkS)
+        (effects (font (size 1 1) (thickness 0.15)) (justify mirror))
+      )
+    `
+
+    const bottom = `
+    )
+    `
+
+    let final = top;
+
+    if(p.side == "F" || p.reverse) {
+      final += front;
+    }
+    if(p.side == "B" || p.reverse) {
+      final += back;
+    }
+
+    final += bottom;
+
+    return final;
+  }
+}
