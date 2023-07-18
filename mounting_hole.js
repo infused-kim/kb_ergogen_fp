2f91af2fed724df3ab7a07fac03c99424802a2e1
Kim Streich <kim@infusedinsight.com>
2023-07-17 22:40:22 -0600

    Mounting Hole FP: Added option to create oval holes

--- mounting_hole.js
+++ mounting_hole.js
@@ -4,29 +4,73 @@
 // Simple mounting hole with gold rim.
 //
 // Parameters:
-//   - size: The total size of the hole, including gold rim (Default: 3.8mm)
+//   - outline: The width of the gold rim around the hole (Default: 0.8mm)
 //   - drill: The actual size for the hole (Default 2.2mm - for m2 screws)
+//   - drill_y: The height if you want an oval hole (Default: off)
 
 module.exports = {
     params: {
       designator: 'H',
-      size: 3.8,
+      outline: 0.8,
       drill: 2.2,
+      drill_y: 0,
     },
     body: p => {
-        const footprint = `
+        if(p.drill_y == 0) {
+            p.drill_y = p.drill
+        }
+
+        const size_x = p.drill + p.outline * 2
+        const size_y = p.drill_y + p.outline * 2
+
+        const courtyard_offset = 0.25
+        const courtyard_x = size_x / 2 + courtyard_offset
+        const courtyard_y = size_y / 2 + courtyard_offset
+
+        const top = `
             (module mounting_hole (layer F.Cu) (tedit 64B5A986)
                 ${p.at /* parametric position */}
                 (fp_text reference "${p.ref}" (at 0 3) (layer F.SilkS) ${p.ref_hide}
                     (effects (font (size 1 1) (thickness 0.15)))
                 )
-                (fp_circle (center 0 0) (end -${p.size / 2 - 0.05} 0) (layer F.CrtYd) (width 0.05))
-                (fp_circle (center 0 0) (end -${p.size / 2 - 0.05} 0) (layer B.CrtYd) (width 0.05))
-                (fp_circle (center 0 0) (end -${p.drill / 2 - 0.05} 0) (layer Dwgs.User) (width 0.05))
-                (pad "" thru_hole circle (at 0 0 180) (size ${p.size} ${p.size}) (drill ${p.drill}) (layers *.Cu *.Mask))
+        `
+
+        const pad_circle = `
+                (pad "" thru_hole circle (at 0 0 180) (size ${size_x} ${size_y}) (drill ${p.drill}) (layers *.Cu *.Mask))
+        `
+        const courtyard_circle = `
+                (fp_circle (center 0 0) (end -${courtyard_x} 0) (layer F.CrtYd) (width 0.05))
+                (fp_circle (center 0 0) (end -${courtyard_x} 0) (layer B.CrtYd) (width 0.05))
+        `
+        const pad_oval = `
+                (pad "" thru_hole oval (at 0 0 180) (size ${size_x} ${size_y}) (drill oval ${p.drill} ${p.drill_y}) (layers *.Cu *.Mask))
+        `
+        const courtyard_oval = `
+                (fp_line (start ${courtyard_x} -${courtyard_y}) (end ${courtyard_x} ${courtyard_y}) (layer F.CrtYd) (width 0.05))
+                (fp_line (start -${courtyard_x} -${courtyard_y}) (end -${courtyard_x} ${courtyard_y}) (layer F.CrtYd) (width 0.05))
+                (fp_line (start -${courtyard_x} ${courtyard_y}) (end ${courtyard_x} ${courtyard_y}) (layer F.CrtYd) (width 0.05))
+                (fp_line (start -${courtyard_x} -${courtyard_y}) (end ${courtyard_x} -${courtyard_y}) (layer F.CrtYd) (width 0.05))
+                (fp_line (start -${courtyard_x} ${courtyard_y}) (end ${courtyard_x} ${courtyard_y}) (layer B.CrtYd) (width 0.05))
+                (fp_line (start -${courtyard_x} ${courtyard_y}) (end -${courtyard_x} -${courtyard_y}) (layer B.CrtYd) (width 0.05))
+                (fp_line (start -${courtyard_x} -${courtyard_y}) (end ${courtyard_x} -${courtyard_y}) (layer B.CrtYd) (width 0.05))
+                (fp_line (start ${courtyard_x} ${courtyard_y}) (end ${courtyard_x} -${courtyard_y}) (layer B.CrtYd) (width 0.05))
+        `
+
+        const bottom = `
             )
         `
 
-        return footprint;
+        let final = top
+        if(size_x == size_y) {
+            final += pad_circle
+            final += courtyard_circle
+        } else {
+            final += pad_oval
+            final += courtyard_oval
+        }
+
+        final += bottom
+
+        return final
     }
 }
