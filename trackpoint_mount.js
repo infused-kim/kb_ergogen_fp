9615349cf1e2cfa089cdc78c86352452a3284879
Kim Streich <kim@infusedinsight.com>
2023-07-18 22:06:38 -0600

    Trackpoint Mount: Added T460S outlines

--- trackpoint_mount.js
+++ trackpoint_mount.js
@@ -17,13 +17,14 @@ module.exports = {
     reverse: false,
 
     // T430: 3.5
-    // T460 (red one): 3.5
+    // T460S (red one): 3.5
     // X240: 5.5
     drill: 5.5,
     outline: 0.25,
 
     show_outline_t430: false,
     show_outline_x240: false,
+    show_outline_t460s: false,
     show_board: false,
   },
   body: p => {
@@ -120,6 +121,50 @@ module.exports = {
         (fp_line (start 12.25 -5.5) (end 12.25 5.5) (layer Dwgs.User) (width 0.2))
     `
 
+    const outline_t460s_front = `
+        (fp_line (start 2.75 6.5) (end 6.25 3) (layer F.Fab) (width 0.2))
+        (fp_line (start 2.75 11.5) (end -2.75 11.5) (layer F.Fab) (width 0.2))
+        (fp_line (start -6.25 3) (end -6.25 -3) (layer F.Fab) (width 0.2))
+        (fp_line (start 6.25 3) (end 6.25 -3) (layer F.Fab) (width 0.2))
+        (fp_line (start 2.75 -11.5) (end -2.75 -11.5) (layer F.Fab) (width 0.2))
+        (fp_line (start 2.75 6.5) (end 2.75 11.5) (layer F.Fab) (width 0.2))
+        (fp_line (start -2.75 6.5) (end -2.75 11.5) (layer F.Fab) (width 0.2))
+        (fp_line (start -2.75 -11.5) (end -2.75 -6.5) (layer F.Fab) (width 0.2))
+        (fp_line (start 2.75 -11.5) (end 2.75 -6.5) (layer F.Fab) (width 0.2))
+        (fp_line (start -2.75 6.5) (end -6.25 3) (layer F.Fab) (width 0.2))
+        (fp_line (start 6.25 -3) (end 2.75 -6.5) (layer F.Fab) (width 0.2))
+        (fp_line (start -6.25 -3) (end -2.75 -6.5) (layer F.Fab) (width 0.2))
+    `
+
+    const outline_t460s_back = `
+        (fp_line (start -6.25 -3) (end -2.75 -6.5) (layer B.Fab) (width 0.2))
+        (fp_line (start 6.25 -3) (end 2.75 -6.5) (layer B.Fab) (width 0.2))
+
+        (fp_line (start 2.75 6.5) (end 6.25 3) (layer B.Fab) (width 0.2))
+        (fp_line (start -2.75 6.5) (end -6.25 3) (layer B.Fab) (width 0.2))
+
+        (fp_line (start 6.25 3) (end 6.25 -3) (layer B.Fab) (width 0.2))
+        (fp_line (start 2.75 11.5) (end -2.75 11.5) (layer B.Fab) (width 0.2))
+        (fp_line (start -6.25 3) (end -6.25 -3) (layer B.Fab) (width 0.2))
+        (fp_line (start 2.75 -11.5) (end -2.75 -11.5) (layer B.Fab) (width 0.2))
+        (fp_line (start -2.75 6.5) (end -2.75 11.5) (layer B.Fab) (width 0.2))
+        (fp_line (start 2.75 6.5) (end 2.75 11.5) (layer B.Fab) (width 0.2))
+        (fp_line (start -2.75 -11.5) (end -2.75 -6.5) (layer B.Fab) (width 0.2))
+        (fp_line (start 2.75 -11.5) (end 2.75 -6.5) (layer B.Fab) (width 0.2))
+    `
+
+    const outline_t460s_board = `
+        (fp_line (start 38.25 12.25) (end 22.25 12.25) (layer Dwgs.User) (width 0.2))
+        (fp_line (start 22.25 2.75) (end 22.25 12.25) (layer Dwgs.User) (width 0.2))
+        (fp_line (start 22.25 -2.75) (end 22.25 2.75) (layer Dwgs.User) (width 0.2))
+        (fp_line (start 22.25 2.75) (end 6.25 2.75) (layer Dwgs.User) (width 0.2))
+        (fp_line (start 22.25 -2.75) (end 6.25 -2.75) (layer Dwgs.User) (width 0.2))
+        (fp_line (start 38.25 -22.25) (end 38.25 12.25) (layer Dwgs.User) (width 0.2))
+        (fp_line (start 38.25 -22.25) (end 22.25 -22.25) (layer Dwgs.User) (width 0.2))
+        (fp_line (start 22.25 -22.25) (end 22.25 -2.75) (layer Dwgs.User) (width 0.2))
+        (fp_line (start 6.25 -2.75) (end 6.25 2.75) (layer Dwgs.User) (width 0.2))
+    `
+
     const size = p.drill + (p.outline * 2)
     const bottom = `
         (pad "" thru_hole circle (at 0 -9.5 180) (size 3.8 3.8) (drill 2.2) (layers *.Cu *.Mask))
@@ -138,12 +183,12 @@ module.exports = {
       }
       if(p.show_outline_x240) {
         final += outline_x240_front;
-
-        if(p.show_board) {
-          final += outline_x240_board
-        }
+      }
+      if(p.show_outline_t460s) {
+        final += outline_t460s_front;
       }
     }
+
     if(p.side == "B" || p.reverse) {
       final += back;
       if(p.show_outline_t430) {
@@ -151,6 +196,18 @@ module.exports = {
       }
       if(p.show_outline_x240) {
         final += outline_x240_back;
+      }
+      if(p.show_outline_t460s) {
+        final += outline_t460s_back;
+      }
+    }
+
+    if(p.show_board) {
+      if(p.show_outline_x240) {
+        final += outline_x240_board
+      }
+      if(p.show_outline_t460s) {
+        final += outline_t460s_board
       }
     }
 
