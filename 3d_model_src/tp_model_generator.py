6aeb471da79d9ffa58e350c63cbb7f0983016358
Kim Streich <kim@infusedinsight.com>
2024-06-22 14:11:32 -0600

    TP 3D Model: Add FPC cable

--- 3d_model_src/tp_model_generator.py
+++ 3d_model_src/tp_model_generator.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import math
 import build123d as bd
 
 
@@ -33,35 +34,53 @@ class TrackPointRedT460S():
         ] = ALIGN_CENTER,
         mode: bd.Mode = bd.Mode.ADD,
     ):
+        self.metal_thickness = 0.4
+        self.metal_frame_width = 12.5
+
+        self.platform_height_total = 1.2
+        self.platform_height_actual = (
+            self.platform_height_total - self.metal_thickness
+        )
+        self.platform_diameter = 8
+        self.platform_radius = 4
+
+        self.adapter_width = 2.2
+        self.adapeter_height = 2.7
+
         return None
 
-    def build_tp(self, z_offset=None):
-        metal_thickness = 0.4
-
-        platform_height_total = 1.2
-        platform_height_actual = platform_height_total - metal_thickness
-        platform_diameter = 8
-
-        adapter_width = 2.2
-        adapeter_height = 2.7
-
-        metal_frame = self._build_sensor_metal_frame(metal_thickness)
-        platform = self._build_sensor_platform(
-            platform_height_actual,
-            platform_diameter,
-            z_offset=metal_thickness,
-        )
-        adapter = self._build_sensor_adapter(
-            adapter_width,
-            adapeter_height,
-            z_offset=(platform_height_total),
-        )
-        platform_screws = self._build_sensor_platform_screws(
-            height=(platform_height_total)
-        )
+    def build_tp(self, pcb_vert_offset=0, z_offset=None):
+        tp_sensor = self.build_tp_sensor(z_offset=z_offset)
+        tp_fpc = self.build_sensor_fpc_cable(pcb_vert_offset)
 
         tp_assembly = bd.Compound(
             label='TrackPoint',
+            children=[
+                tp_sensor,
+                tp_fpc,
+            ]
+        )
+
+        return tp_assembly
+
+    def build_tp_sensor(self, z_offset=None):
+        metal_frame = self._build_sensor_metal_frame(self.metal_thickness)
+        platform = self._build_sensor_platform(
+            self.platform_height_actual,
+            self.platform_diameter,
+            z_offset=self.metal_thickness,
+        )
+        adapter = self._build_sensor_adapter(
+            self.adapter_width,
+            self.adapeter_height,
+            z_offset=(self.platform_height_total),
+        )
+        platform_screws = self._build_sensor_platform_screws(
+            height=(self.platform_height_total)
+        )
+
+        tp_sensor_assembly = bd.Compound(
+            label='TrackPoint Sensor',
             children=[
                 metal_frame,
                 platform,
@@ -72,10 +91,192 @@ class TrackPointRedT460S():
 
         if z_offset is None:
             pcb_height_default = 1.6
-            z_offset = -(platform_height_total + pcb_height_default)
-        tp_assembly.move(bd.Location((0, 0, z_offset)))
+            z_offset = -(self.platform_height_total + pcb_height_default)
+        tp_sensor_assembly.move(bd.Location((0, 0, z_offset)))
 
-        return tp_assembly
+        return tp_sensor_assembly
+
+    def build_sensor_fpc_cable(self, pcb_vertical_offset=0):
+        fpc_thickness = 0.1
+
+        fpc_width_sensor = 3.0
+        fpc_width_sensor_h = fpc_width_sensor / 2
+        fpc_width = 5.0
+        fpc_width_h = fpc_width / 2
+        fpc_width_pcb = 11
+        fpc_width_pcb_h = fpc_width_pcb / 2
+
+        fpc_len_widening = 1.0
+        fpc_len_between_sensor_pcb = 15.0
+
+        # We always need to bring the fpc cable down from the metal part.
+        # And on top of that the pcb can also be offset.
+        fpc_vertical_offset = (
+            0 - self.metal_thickness + pcb_vertical_offset
+        )
+        # The points between the metal frame and pcb that the
+        # fpc cable connects (on the xz axes)
+        point_fpc_main_start_xz = (
+            self.metal_frame_width / 2,
+            self.metal_thickness
+        )
+        point_fpc_main_end_xz = self._calc_offset_point(
+            start_point=point_fpc_main_start_xz,
+            line_length=fpc_len_between_sensor_pcb,
+            vertical_offset=fpc_vertical_offset,
+        )
+        point_fpc_main_widening = self._calc_point_on_line_at_length(
+            length=fpc_len_widening,
+            point_a=point_fpc_main_start_xz,
+            point_b=point_fpc_main_end_xz,
+        )
+
+        fpc_len_on_pcb = 4.0
+        pcb_offset_cable = (
+            point_fpc_main_end_xz[0] + fpc_len_on_pcb
+        )
+
+        fpc_contacts_len = 0.75
+        fpc_contacts_width = 0.5
+        fpc_contacts_spacing = 2.0
+        fpc_contacts_count = 4
+
+        pcb_offset_cable_contacts = pcb_offset_cable + fpc_contacts_len
+
+        fpc_line = [
+            (
+                -self.platform_radius,
+                0,
+                self.platform_height_total
+            ),
+            (
+                0,
+                0,
+                self.platform_height_total
+            ),
+            (
+                0,
+                fpc_width_sensor_h,
+                self.platform_height_total
+            ),
+            (
+                self.platform_radius,
+                fpc_width_sensor_h,
+                self.platform_height_total
+            ),
+            (
+                self.platform_radius + 0.7,
+                fpc_width_sensor_h,
+                self.metal_thickness
+            ),
+            (
+                point_fpc_main_start_xz[0],
+                fpc_width_sensor_h,
+                point_fpc_main_start_xz[1]
+            ),
+            (
+                point_fpc_main_widening[0],
+                fpc_width_h,
+                point_fpc_main_widening[1]
+            ),
+            (
+                point_fpc_main_end_xz[0],
+                fpc_width_h,
+                point_fpc_main_end_xz[1]
+            ),
+            (
+                point_fpc_main_end_xz[0],
+                fpc_width_pcb_h,
+                point_fpc_main_end_xz[1]
+            ),
+            (
+                pcb_offset_cable_contacts,
+                fpc_width_pcb_h,
+                point_fpc_main_end_xz[1]
+            ),
+            (
+                pcb_offset_cable_contacts,
+                0,
+                point_fpc_main_end_xz[1]
+            )
+        ]
+
+        fpc_points_xz = self._get_points_for_axes(fpc_line, 'x', 'z')
+        fpc_points_xy = self._get_points_for_axes(fpc_line, 'x', 'y')
+
+        with bd.BuildPart() as fpc:
+            with bd.BuildLine(bd.Plane.XZ):
+                bd.Polyline(fpc_points_xz)
+            bd.make_brake_formed(
+                thickness=fpc_thickness,
+                station_widths=fpc_width * 2,
+                side=bd.Side.LEFT,
+            )
+            bd.mirror(about=bd.Plane.XZ)
+
+            with bd.BuildSketch(bd.Plane.XY.offset(5)):
+
+                # We create a large rectangle that will subtract from the cable
+                bd.Rectangle(
+                    width=100,
+                    height=50,
+                )
+
+                # Then we cut holes into that rectangle, which will not be
+                # subsacted from the FPC cable, such as the platform circle
+                bd.Circle(
+                    self.platform_radius,
+                    mode=bd.Mode.SUBTRACT,
+                )
+
+                # And the XY outline of the cable
+                with bd.BuildLine() as ln:
+                    bd.Polyline(fpc_points_xy)
+                bd.mirror(ln.line, about=bd.Plane.XZ)
+                bd.make_face(mode=bd.Mode.SUBTRACT)
+
+                # And the solder contacts
+                with bd.Locations((pcb_offset_cable, 0)):
+                    # First fill the stencil with a rect where the contacts
+                    # will be
+                    bd.Rectangle(
+                        width=fpc_contacts_len,
+                        height=fpc_width_pcb,
+                        align=(bd.Align.MIN, bd.Align.CENTER),
+                        mode=bd.Mode.ADD,
+                    )
+
+                    # And then cut out the actual contacts from it
+                    solder_contact_locs = bd.GridLocations(
+                        x_spacing=0,
+                        y_spacing=fpc_contacts_spacing,
+                        x_count=1,
+                        y_count=fpc_contacts_count,
+                    )
+                    with solder_contact_locs:
+                        bd.Rectangle(
+                            width=fpc_contacts_len,
+                            height=fpc_contacts_width,
+                            align=(bd.Align.MIN, bd.Align.CENTER),
+                            mode=bd.Mode.SUBTRACT,
+                        )
+
+                bd.Rectangle(
+                    width=self.adapter_width + 0.2,
+                    height=self.adapter_width + 0.2,
+                )
+            bd.extrude(
+                amount=20,
+                both=True,
+                mode=bd.Mode.SUBTRACT,
+            )
+        # TODO: Figure out proper way to set correct location
+        fpc.part.move(bd.Location((0, 0, -self.adapeter_height)))
+
+        fpc.part.color = bd.Color('orange')
+        fpc.part.label = 'TrackPoint FPC Cable'
+
+        return fpc.part
 
     def _build_sensor_metal_frame(self, metal_thickness):
 
@@ -189,7 +390,7 @@ class TrackPointRedT460S():
             ])
 
         metal_frame.part.color = COLOR_SILVER
-        metal_frame.part.label = 'TrackPoint Sensor - Metal Frame'
+        metal_frame.part.label = 'Metal Frame'
 
         return metal_frame.part
 
@@ -212,7 +413,7 @@ class TrackPointRedT460S():
             )
 
         platform.part.color = bd.Color('black')
-        platform.part.label = 'TrackPoint Sensor - Round Platform'
+        platform.part.label = 'Round Platform'
 
         return platform.part
 
@@ -227,7 +428,7 @@ class TrackPointRedT460S():
                 )
 
         adapter.part.color = bd.Color('white')
-        adapter.part.label = 'TrackPoint Sensor - White Cap Adapter'
+        adapter.part.label = 'White Cap Adapter'
 
         return adapter.part
 
@@ -244,3 +445,69 @@ class TrackPointRedT460S():
         screws.part.label = 'TrackPoint Sensos - Platform Screws'
 
         return screws.part
+
+    def _calc_offset_point(self, start_point, line_length, vertical_offset):
+        pythagoras_a = vertical_offset
+        pythagoras_c = line_length
+
+        if pythagoras_a > pythagoras_c:
+            raise ValueError(
+                'The vertical offset cannot be longer than the diagonal line '
+                ' length'
+            )
+
+        pythagoras_b = math.sqrt(pythagoras_c**2 - pythagoras_a**2)
+
+        start_point_x, start_point_y = start_point
+        offset_point = (
+            start_point_x + pythagoras_b,
+            start_point_y + pythagoras_a
+        )
+
+        return offset_point
+
+    def _calc_point_on_line_at_length(self, length, point_a, point_b):
+        x1, y1 = point_a
+        x2, y2 = point_b
+
+        total_distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+        if length > total_distance:
+            raise ValueError(
+                'The specified length is greater than the distance between '
+                'the points.'
+            )
+
+        proportion = length / total_distance
+
+        x = x1 + proportion * (x2 - x1)
+        y = y1 + proportion * (y2 - y1)
+
+        return (x, y)
+
+    def _get_points_for_axes(self, points, axis_1, axis_2):
+        allowed_axes = ['x', 'y', 'z']
+        if axis_1 not in allowed_axes or axis_2 not in allowed_axes:
+            raise ValueError(
+                f'Axes have to be one of these: {", ".join(allowed_axes)}'
+            )
+
+        index_1 = allowed_axes.index(axis_1)
+        index_2 = allowed_axes.index(axis_2)
+
+        new_points = []
+        for point in points:
+            point_new = (point[index_1], point[index_2])
+
+            # If the point is the same as the previous point, then
+            # skip it
+            try:
+                prev_point = new_points[-1]
+                if point_new == prev_point:
+                    continue
+            except IndexError:
+                pass
+
+            new_points.append(point_new)
+
+        return new_points
